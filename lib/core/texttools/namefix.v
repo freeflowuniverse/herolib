6f814b5d09e67d8a746239c0cfb56aa560fdf106
timurgordon <timurgordon@gmail.com>
2025-01-02 01:47:56 -0500

    add snake_case name fixer

--- lib/core/texttools/namefix.v
+++ lib/core/texttools/namefix.v
@@ -84,6 +84,11 @@ pub fn name_fix_no_underscore(name string) string {
 	return x
 }
 
+pub fn name_fix_snake(name string) string {
+	name_ := name_fix_dot_notation_to_pascal(name)
+	return name_fix_pascal_to_snake(name_)
+}
+
 pub fn name_fix_snake_to_pascal(name string) string {
 	x := name.replace('_', ' ')
 	p := x.title().replace(' ', '')
