cb8c550ed1b8f927c97cae9d238bdab61791c24b
despiegk <kristof@incubaid.com>
2025-02-07 12:38:49 +0300

    chore: bump version to 1.0.5

--- cli/hero.v
+++ cli/hero.v
@@ -31,7 +31,7 @@ fn do() ! {
 	mut cmd := Command{
 		name:        'hero'
 		description: 'Your HERO toolset.'
-		version:     '1.0.4'
+		version:     '1.0.5'
 	}
 
 	// herocmds.cmd_run_add_flags(mut cmd)
