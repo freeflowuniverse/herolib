a55af220bfee4da78d31a4314a88fd260d95b5da
despiegk <kristof@incubaid.com>
2025-03-13 10:56:55 +0100

    ...

--- lib/mcp/v_do/handlers/vrun.v
+++ lib/mcp/v_do/handlers/vrun.v
@@ -2,3 +2,7 @@ module handlers
 
 import os
 import freeflowuniverse.herolib.mcp.v_do.logger
+
+
+
+cmd := 'v -gc none -stats -enable-globals -show-c-output -keepc -n -w -cg -o /tmp/tester.c -g -cc tcc ${fullpath}'
