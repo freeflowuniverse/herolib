39c6c37dee15ae91613a7b1ee89027d6b84edcae
despiegk <kristof@incubaid.com>
2025-05-04 20:23:12 +0300

    bump version to 1.0.25

--- cli/hero.v
+++ cli/hero.v
@@ -51,7 +51,7 @@ fn do() ! {
 	mut cmd := Command{
 		name:        'hero'
 		description: 'Your HERO toolset.'
-		version:     '1.0.22'
+		version:     '1.0.25'
 	}
 
 	// herocmds.cmd_run_add_flags(mut cmd)
--- install_hero.sh
+++ install_hero.sh
@@ -4,7 +4,7 @@ set -e
 
 os_name="$(uname -s)"
 arch_name="$(uname -m)"
-version='1.0.22'
+version='1.0.25'
 
 
 # Base URL for GitHub releases
