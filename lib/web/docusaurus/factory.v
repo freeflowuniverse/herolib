f9bdb22c677d93967a6198c69dec7c1508627201
despiegk <kristof@incubaid.com>
2025-05-19 05:44:23 +0400

    ...

--- lib/web/docusaurus/factory.v
+++ lib/web/docusaurus/factory.v
@@ -5,8 +5,8 @@ import os
 import freeflowuniverse.herolib.core.pathlib
 // import freeflowuniverse.herolib.ui.console
 // import freeflowuniverse.herolib.core.base
-import freeflowuniverse.herolib.develop.gittools
-import freeflowuniverse.herolib.ui.console
+// import freeflowuniverse.herolib.develop.gittools
+// import freeflowuniverse.herolib.ui.console
 
 @[heap]
 pub struct DocusaurusFactory {
@@ -43,28 +43,6 @@ pub fn new(args_ DocusaurusArgs) !&DocusaurusFactory {
 		args:       args_
 		path_build: pathlib.get_dir(path: args.build_path, create: true)!
 		// path_publish: pathlib.get_dir(path: args_.publish_path, create: true)!
-	}
-
-	// Process HeroScript
-	mut heroscript_text := args.heroscript
-	mut heroscript_path := args.heroscript_path
-
-	// If no heroscript is explicitly provided, check current directory
-	if heroscript_text == '' && heroscript_path == '' {
-		// First check if there's a .heroscript file in the current directory
-		current_dir := os.getwd()
-		cfg_dir := os.join_path(current_dir, 'cfg')
-		if os.exists(cfg_dir) {
-			heroscript_path = cfg_dir
-		}
-	}
-
-	// Process any HeroScript that was found
-	if heroscript_text != '' || heroscript_path != '' {
-		ds.config = play(
-			heroscript:      heroscript_text
-			heroscript_path: heroscript_path
-		)!
 	}
 
 	ds.template_install(install: true, template_update: args.update, delete: true)!
