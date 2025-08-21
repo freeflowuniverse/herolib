2a38221d7f4399748025594e649a9595e7536e30
despiegk <kristof@incubaid.com>
2025-08-21 12:27:48 +0200

    bump version to 1.0.29

--- cli/hero.v
+++ cli/hero.v
@@ -48,7 +48,7 @@ fn do() ! {
 	mut cmd := Command{
 		name:        'hero'
 		description: 'Your HERO toolset.'
-		version:     '1.0.28'
+		version:     '1.0.29'
 	}
 
 	// herocmds.cmd_run_add_flags(mut cmd)
